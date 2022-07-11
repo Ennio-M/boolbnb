960d376978e6583b2d0ad45ed149b7b54e1736bf
Ennio-M <marrese.ennio@gmail.com>
2022-07-11 18:19:41 +0200

    inizio impostazione layouts crud

--- public/js/app.js
+++ public/js/app.js
@@ -49562,8 +49562,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\boolean\codice\progetto_finale\boolbnb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\boolean\codice\progetto_finale\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\BOOLEAN\Codice\Esercitazioni\progetto_finale\boolbnb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\BOOLEAN\Codice\Esercitazioni\progetto_finale\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })

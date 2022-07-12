f211fc76f3e346db054b83d7bf29743f604b20a8
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-07-12 12:32:40 +0200

    check 1

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,19 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css').options({
-        processCssUrls: false
-     }).copyDirectory( './node_modules/@fortawesome/fontawesome-free/webfonts/*', 'public/fonts/font-awesome' );
+mix.js("resources/js/admin.js", "public/js")
+    .sass("resources/sass/admin.scss", "public/css")
+    .options({
+        processCssUrls: false,
+    })
+    .copyDirectory(
+        "./node_modules/@fortawesome/fontawesome-free/webfonts/*",
+        "public/fonts/font-awesome"
+    );
+
+//frontoffice
+mix.js("resources/js/front.js", "public/js")
+    .sass("resources/sass/front.scss", "public/css")
+    .options({
+        processCssUrls: false,
+    });

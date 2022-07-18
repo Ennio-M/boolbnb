bd05f8761a8423a96b82a8d0449c308e43ab14d9
GiulianoCretella <giulianocretella.1@gmail.com>
2022-07-18 18:27:49 +0200

    apartament view

--- resources/js/pages/ApartmentComponent.vue
+++ resources/js/pages/ApartmentComponent.vue
@@ -1,32 +1,41 @@
 <template>
     <main>
-        <section class="container py-5 text-center">
+        <section class="container py-5">
             <div class="row justify-content-center">
-                <div class="col-12">
+                <div class="col-12 font-weigth-bold text-center">
                     <h1>{{ apartment.title }}</h1>
                 </div>
-                <div class="col-12">
-                    <div class="slider-wrapper" tabindex="0"
+                <div class="col-12 h-75">
+                    <div class="slider-wrapper rounded" tabindex="0"
                     @keydown.left="slidePrev"
                     @keydown.right="slideNext"
                     >
-                        <div class="item h-50" >
-                            <img class="img-fluid" :src="`/storage/${apartment.images[indexActive].image}`" :alt="apartment.title" />
+                        <div class="item position-relative" >
+                            <div @click="slidePrev" class="prev">prev</div>
+                            <div @click="slideNext" class="next">next</div>
+                            <img class="img-fluid rounded" :src="`/storage/${apartment.images[indexActive].image}`" :alt="apartment.title" />
                         </div>
 
-                        <div class="thumbs d-flex">
-                            <div @click="slidePrev" class="prev translate-middle"></div>
-                            <div @click="slideNext" class="next translate-middle"></div>
+                        <div class="thumbs d-flex rounded my-3">
                             <div @click="select(index)" class="thumb" :class="{'active': index === indexActive}" v-for="(images,index) in apartment.images" :key="index">
-                                <img :src="`/storage/${images.image}`" :alt="apartment.title"/>
+                                <img class="rounded-bottom" :src="`/storage/${images.image}`" :alt="apartment.title"/>
                             </div>
                         </div>
 
                     </div>
                 </div>
                 
-                <div class="col-12">
+                <div class="col-12 py-2">
+                   <h5>Intero Alloggio - Host : {{apartment.user_id}}  </h5>
+                   <span>{{apartment.beds * 2}} Ospiti - {{apartment.
+                    rooms}} camere da letto - {{apartment.beds}} letti - {{apartment.bathrooms}} bagni</span>
                    <p v-html="apartment.description"></p>
+                </div>
+                <div class="col-12">
+                    <h3>Cosa Troverai</h3>
+                    <div v-for="(service,index) in apartment.services" :key="index">
+                        <p>{{service.name}}</p>
+                    </div>
                 </div>
             </div>
            <!-- <map-component :apartment="apartment" /> -->
@@ -76,72 +85,60 @@ export default {
 
 <style scoped lang="scss">
 .slider-wrapper{
-    
-}
-.item{
-    position: relative;
-    img{
-        height: 100%;
-    }
-}
-.thumbs {
-    width: 100%;
-    background: #000;
-    position: relative;
-    .thumb {
-      width: calc(100% / 5);
-      opacity: 0.5;
-      img {
+    outline:0;
+    .item{
+        position: relative;
+        overflow: hidden;
+        img{
+            height: 100%;
+        }
+        .prev, .next {
+            text-align: center;
+            width: 100px;
+            height: 100px;
+            margin: 10px 0;
+            border-radius: 50%;
+            background: white;
+            position: absolute;
+            cursor: pointer;
+            z-index: 999;
+            line-height: 25px;
+            text-transform: uppercase;
+            padding: 10px;
+            font-size: 1em;
+            letter-spacing: 1.5px;
+            color:#E61C54;
+            top: 50%;
+        }
+        .next{
+            transform: rotate(270deg);
+            right: -60px;
+        }
+        .prev{
+            left: -60px;
+            transform: rotate(90deg);
+        }   
+    }   
+    .thumbs {
         width: 100%;
-        height: 100%;
-        object-fit: cover;
-      }
+        background: #000;
+        .thumb {
+            width: calc(100% / 5);
+            opacity: 0.5;
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+            }
+        }
+        .thumb.active {
+            border: 2px solid #ccc;
+            opacity: 1;
+        }
     }
-  }
-  .thumb.active {
-      border: 2px solid #ccc;
-      opacity: 1;
-  }
-
-  .prev, .next {
-      width: 20px;
-      height: 20px;
-      margin: 10px 0;
-      border-radius: 50%;
-      background: #ccc;
-      position: absolute;
-      right: 0;
-      bottom: 50%;
-      cursor: pointer;
-      z-index: 999;
-  }
-
-  .next {
-      left: 0;
-  }
-  .prev::after {
-      content: '';
-      width: 10px;
-      height: 10px;
-      border-top: 1px solid black;
-      border-right: 1px solid black;
-      display: block;
-      position: absolute;
-      top: 35%;
-      left: 50%;
-      transform: translate(-50%) rotate(-45deg);
-  }
-
-  .next::before {
-      content: '';
-      width: 10px;
-      height: 10px;
-      border-top: 1px solid black;
-      border-right: 1px solid black;
-      display: block;
-      position: absolute;
-      bottom: 35%;
-      left: 50%;
-      transform: translate(-50%) rotate(135deg);
-  }
-</style>
+     .description_container{
+        border-bottom: 3px solid grey;
+    }
+ }
+   
+ </style>

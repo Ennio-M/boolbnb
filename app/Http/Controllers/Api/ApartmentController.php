0b677ec347ff2ae5ff7f65dcb379a74a03b7d94d
Ennio-M <marrese.ennio@gmail.com>
2022-07-19 13:28:17 +0200

    lista appartamenti distanza inferiore a input

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -18,6 +18,8 @@ class ApartmentController extends Controller
      */
     public function index($userInput, $userRange)
     {   
+        // salvo tutti gli appartamenti
+        $apartments = Apartment::all();
         //chiamo l'api di tomtom passandole l'indirizzo inserito dall'utente
         $user_response = Http::get('https://api.tomtom.com/search/2/structuredGeocode.json', [
             'key' => $this->myTomTomApiKey,
@@ -28,50 +30,69 @@ class ApartmentController extends Controller
         //salvo le coordinate che l'api mi ha restituito
         $user_lat = $user_decoded->results[0]->position->lat;
         $user_lon = $user_decoded->results[0]->position->lon;
-        
-        $data = [];
-        //definisco il tipo di ricerca che voglio effettuare e il suo raggio
-        $geometryList_array = [[
-            "type" => "CIRCLE",
-            "position" => "$user_lat, $user_lon",
-            "radius" => $userRange * 1000
-        ]];
-        $geometryList = json_encode($geometryList_array);
-        //salvo tutti gli appartamenti del db
-        $apartments = Apartment::all();
-        $apts = response()->json($apartments);
-        //codifico in json la lista di tutti gli appartamenti del db
-        foreach($apts->original as $apt){
-            $item = [
-                "apartment" => [
-                    "id" => $apt->id
-                ],
-                "position" => [
-                    "lat" => $apt->latitude,
-                    "lon" => $apt->longitude
-                ]
-            ];
-            array_push($data, $item);
-        }
-        $data_encoded = json_encode($data);
 
-        //chiamo l'api di tomtom passandole il tipo di ricerca e il json degli appartamenti
-        $response = Http::get('https://api.tomtom.com/search/2/geometryFilter.json', [
-            'key' => $this->myTomTomApiKey,
-            'geometryList' => $geometryList,
-            'poiList' => $data_encoded,
-        ]);
-        //in $decoded ci sono gli id degli appartamenti che rientrano nel raggio di ricerca impostato
-        $decoded = json_decode($response->body());
-        //con un ciclo foreach seleziono dal db tutti gli appartamenti interessati
-        $apartmentsList = [];
-        foreach($decoded->results as $apartment){
-            array_push($apartmentsList, 
-                Apartment::where("id", $apartment->apartment->id)->with(["images", "services"])->first()
-            );
+
+        $inRangeApts = [];
+
+        foreach($apartments as $apartment){
+            $theta = $user_lon - $apartment->longitude;
+            $distance =  (sin(deg2rad($user_lat)) * sin(deg2rad($apartment->latitude))) + (cos(deg2rad($user_lat)) * cos(deg2rad($apartment->latitude)) * cos(deg2rad($theta)));
+            $distance = acos($distance);
+            $distance = rad2deg($distance);
+            $distance = $distance * 60 * 1.1515 * 1.609344;
+            $distance = round($distance, 2);
+            if($distance < $userRange){
+                array_push($inRangeApts, $apartment);
+            }
         }
-        //restituisco una risposta in json
-        return response()->json($apartmentsList);
+
+        return $inRangeApts;
+
+
+        
+        // $data = [];
+        // //definisco il tipo di ricerca che voglio effettuare e il suo raggio
+        // $geometryList_array = [[
+        //     "type" => "CIRCLE",
+        //     "position" => "$user_lat, $user_lon",
+        //     "radius" => $userRange * 1000
+        // ]];
+        // $geometryList = json_encode($geometryList_array);
+        // //salvo tutti gli appartamenti del db
+        // $apartments = Apartment::all();
+        // $apts = response()->json($apartments);
+        // //codifico in json la lista di tutti gli appartamenti del db
+        // foreach($apts->original as $apt){
+        //     $item = [
+        //         "apartment" => [
+        //             "id" => $apt->id
+        //         ],
+        //         "position" => [
+        //             "lat" => $apt->latitude,
+        //             "lon" => $apt->longitude
+        //         ]
+        //     ];
+        //     array_push($data, $item);
+        // }
+        // $data_encoded = json_encode($data);
+
+        // //chiamo l'api di tomtom passandole il tipo di ricerca e il json degli appartamenti
+        // $response = Http::get('https://api.tomtom.com/search/2/geometryFilter.json', [
+        //     'key' => $this->myTomTomApiKey,
+        //     'geometryList' => $geometryList,
+        //     'poiList' => $data_encoded,
+        // ]);
+        // //in $decoded ci sono gli id degli appartamenti che rientrano nel raggio di ricerca impostato
+        // $decoded = json_decode($response->body());
+        // //con un ciclo foreach seleziono dal db tutti gli appartamenti interessati
+        // $apartmentsList = [];
+        // foreach($decoded->results as $apartment){
+        //     array_push($apartmentsList, 
+        //         Apartment::where("id", $apartment->apartment->id)->with(["images", "services"])->first()
+        //     );
+        // }
+        // //restituisco una risposta in json
+        // return response()->json($apartmentsList);
     }
 
     /**

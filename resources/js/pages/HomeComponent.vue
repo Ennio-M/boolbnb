9c0b8fabe56010b5d75c631fc4470ad77a7a9467
GiulianoCretella <giulianocretella.1@gmail.com>
2022-07-22 18:52:00 +0200

    modifiche homepage e header

--- resources/js/pages/HomeComponent.vue
+++ resources/js/pages/HomeComponent.vue
@@ -3,15 +3,21 @@
   <section class="jumbo text-center">
     <div class="text-jumbo">
       <h2>Cerca l'appartamento dei tuoi sogni!</h2>
-      <p>BoolBnB da oggi ti offre l'opportunità di trovare un alloggio che soddisfi a pieno le tue esigenze </p>
+      <p>BoolBnB da oggi ti offre l'opportunità di trovare un alloggio che soddisfi a pieno le tue esigenze!</p>
     </div>
   </section>
+  <h2 class="display-6 text-center py-2">Appartamenti in evidenza</h2>
   <div class="container">
       <div class="row justify-content-center">
-        <div class="col-10" v-for="(apartment,index) in apartments" :key="index">
-          <div class="card">
-            <img class="img-fluid" :src="`../storage/${apartment.images[0].image}`" alt="">
-          </div>
+        <div class="col-10 col-md-6 col-lg-4 col-xl-3 p-2" v-for="(apartment,index) in apartments" :key="index">
+          <router-link :to="{ name: 'apartment', params: { slug: apartment.slug } }">
+            <div class="card-box">
+              <img class="rounded" :src="`../storage/${apartment.images[0].image}`" alt="">
+              <h6 class="font-weight-bold pt-1">{{apartment.title}}</h6>
+              <span>Host privato</span>
+              <p class="p-0"><span class="font-weight-bold">{{apartment.price}}</span> €/ notte</p>
+            </div>
+          </router-link>
         </div>
       </div>
     </div>
@@ -47,6 +53,14 @@ export default {
     position:absolute;
     bottom: 0;
     right: 5%;
+  }
+}
+.card-box{
+  text-decoration: none;
+  color: black;
+  img{
+    height: 250px;
+    width: 100%;
   }
 }
 </style>
